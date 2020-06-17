fb63f84dceb9e40d218e1819f941fcbb9772aabc
Aaron Shafovaloff <ashafovaloff@instructure.com>
2020-06-17 12:17:55 -0600

    Fix docs

---
+++ docs/precache-manifest.5e703430256b37b887593621ef6c0b7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89681d5a2e462977f407d9139186817e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "725f32b289f14c23a1a6",
+    "url": "./static/js/2.80797e8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.80797e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc124214d78d2dc95579",
+    "url": "./static/js/main.1612c230.chunk.js"
+  },
+  {
+    "revision": "044246bea37b24d8cc90",
+    "url": "./static/js/runtime-main.5ac5058a.js"
+  }
+]);
